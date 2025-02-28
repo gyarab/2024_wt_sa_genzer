cb4c6775fa7bfbf8d527f2900cdda6a80e373fad
O-takar <otakarg@centrum.cz>
2025-02-28 08:53:55 +0100

    zelva

---
+++ zelva.py
@@ -0,0 +1,21 @@
+from math import sqrt
+from turtle import *
+
+scale = 200
+diagonal = sqrt(2*(scale**2))
+forward(scale)
+left(135)
+forward(diagonal)
+left(135)
+forward(scale)
+left(135)
+forward(diagonal)
+left(90)
+forward(diagonal / 2)
+left(90)
+forward(diagonal / 2)
+left(135)
+forward(scale)
+right(90)
+forward(scale)
+exitonclick()
